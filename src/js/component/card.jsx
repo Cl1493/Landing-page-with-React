fee04b5b1fafabff71d64a6b7ae47bc747dba28a
Cl1493 <152617155+Cl1493@users.noreply.github.com>
2024-03-12 17:09:41 +0000

    funcion map, cartas

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,41 +1,23 @@
 import React from "react";
 
-const Card = () => {
-	return (
-    <div className="d-flex flex-column flex-md-row justify-content-center">
-        <div className="card pr-1 pl-1" style={{width: '18rem', margin: '0.5em'}}>
-        <img src="https://picsum.photos/500/325" className="card-img-top" alt="..."/>
-            <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
+const Card = (props) => {
+
+    return(
+        <div className="d-flex flex-md-row justify-content-center">
+                <div className="card pr-1 pl-1" style={{width: '18rem', margin: '0.5em'}}>
+                        <img src={props.image} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.text}</p>
+                        <a href={props.link} className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
         </div>
-         <div className="card" style={{width: '18rem', margin: '0.5em'}}>
-         <img src="https://picsum.photos/500/325" className="card-img-top" alt="..."/>
-             <div className="card-body">
-             <h5 className="card-title">Card title</h5>
-             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             <a href="#" className="btn btn-primary">Go somewhere</a>
-             </div>
-         </div>
-          <div className="card" style={{width: '18rem', margin: '0.5em'}}>
-          <img src="https://picsum.photos/500/325" className="card-img-top" alt="..."/>
-              <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-          </div>
-           <div className="card" style={{width: '18rem', margin: '0.5em'}}>
-           <img src="https://picsum.photos/500/325" className="card-img-top" alt="..."/>
-               <div className="card-body">
-               <h5 className="card-title">Card title</h5>
-               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-               <a href="#" className="btn btn-primary">Go somewhere</a>
-               </div>
-           </div>
-    </div>
-	);
+    );
 };
+
+
+
+
+
 export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,15 +3,37 @@ import React from "react";
 //include images into your bundle
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
-import Card from "./card";
 import Footer from "./footer";
+import Card from "./card";
 //create your first component
 const Home = () => {
+	const Cartas = [{image:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg",
+	title:"Titulo 1",
+	text:"Este es un texto de prueba",
+	link:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg"
+},{image:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg",
+title:"Titulo 2",
+text:"Este es un texto de prueba, pero no mucho texto",
+link:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg"
+},{image:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg",
+title:"Titulo 3",
+text:"Este es un texto de prueba, con más texto",
+link:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg"
+},{image:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg",
+title:"Titulo 4",
+text:"Este es un texto de prueba, y otro con no mucho",
+link:"https://previews.123rf.com/images/shushanto/shushanto2209/shushanto220900703/191842443-imagen-de-fondo-de-la-ilustraci%C3%B3n-del-arte-conceptual-de-la-destrucci%C3%B3n-de-los-planetas.jpg"
+}
+]
 	return (
 		<div>
 		<Navbar />
 		<Jumbotron />
-		<Card />
+		<div style={{display: "flex", justifyContent: "center"}}>
+		{Cartas.map((carta,index)=>(
+			<Card image={carta.image} title={carta.title} text={carta.text} link={carta.link} key={index}/>
+		))}  
+		</div>
 		<Footer />
 		</div>
 	);
